df8c0cabe36f929d84fd8e298f0f019a7a70ac29
Marybeth Snodgrass <msnodgrass25@gmail.com>
2016-03-14 19:39:58 -0500

    initialized routes for driver and group files

--- routes/driver.js
+++ routes/driver.js
@@ -6,3 +6,13 @@ const router = express.Router();
 const Driver = require('../models/driver');
 
 const ctrl = require('../controllers/driver');
+
+router
+  .get('/drivers', ctrl.index)
+  .post('/drivers', ctrl.create)
+  .get('/drivers/:id', ctrl.show)
+  .get('/drivers/:id/edit', ctrl.edit)
+  .put('/drivers/:id', ctrl.update)
+  .delete('/drivers/:id', ctrl.destroy);
+
+module.exports = router;
--- routes/group.js
+++ routes/group.js
@@ -6,3 +6,13 @@ const router = express.Router();
 const Group = require('../models/group');
 
 const ctrl = require('../controllers/group');
+
+router
+  .get('/groups', ctrl.index)
+  .post('/groups', ctrl.create)
+  .get('/groups/:id', ctrl.show)
+  .get('/groups/:id/edit', ctrl.edit)
+  .put('/groups/:id', ctrl.update)
+  .delete('/groups/:id', ctrl.destroy);
+
+module.exports = router;
