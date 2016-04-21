87b6c3934b5bac79dcf0d5d07329bde9e31993e3
Marybeth Snodgrass <msnodgrass25@gmail.com>
2016-04-21 14:42:10 -0500

    functionality working where if a field is not being edited, the old user info is kept. need to program this to edit now though, and to do this for driver too

--- public/ctrlsAndViews/myaccount/myaccount.js
+++ public/ctrlsAndViews/myaccount/myaccount.js
@@ -1,13 +1,15 @@
 
   'use strict';
   app.controller('myaccountCtrl', ["$scope", "$timeout", "$location", "$http", function ($scope, $timeout, $location, $http) {
-    $scope.newEmail = '';
     $scope.newPhone = '';
     $scope.newAddress = '';
     $scope.newCity = '';
     $scope.newState = '';
     $scope.newZip = '';
+    $scope.newEmail = '';
+    $scope.newGroupName = '';
     let phone = '';
+    let newUserData = {};
 
     $http.get('/user/:id')
     .success(function(user) {
@@ -25,18 +27,29 @@
         console.log("there was an error")
     });
 
-    $scope.updatePhone = function () {
-        phone = $scope.newPhone 
-        // || $scope.userData.user.phone;
-        console.log("phone", phone);
-        $http.put('/user/:id', {phone:phone})
-        .then( function (response, data) {
-            console.log("phone number was updated to ", phone);
-        }),  function(response) {
-            console.log("there was an error")
+    $scope.updateAcctData = function () {
+        console.log("$scope.userData", $scope.userData);
+        newUserData = {
+            phone:$scope.newPhone || $scope.userData.user.phone,
+            address: $scope.newAddress || $scope.userData.user.address,
+            city: $scope.newCity || $scope.userData.user.city,
+            state: $scope.newState || $scope.userData.user.state,
+            zip: $scope.newZip || $scope.userData.user.zip,
+            email: $scope.newEmail || $scope.userData.user.email,
+            groupName: $scope.newGroupName || $scope.userData.user.groupName
         }
-        $scope.userData.phone = $scope.newPhone;
-        $scope.newPhone = '';
+        console.log("newUserData", newUserData);
+
+
+        // $http.put('/user/:id', {phone:phone})
+        // .then( function (response, data) {
+        //     console.log("phone number was updated to ", phone);
+        // }),  function(response) {
+        //     console.log("there was an error")
+        // }
+        // $scope.userData.phone = $scope.newPhone;
+        // $scope.newPhone = '';
     };
+
 
 }]);
